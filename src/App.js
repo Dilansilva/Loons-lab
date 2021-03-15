cdbeb6de1f0d47cf24af63fe5b89f44cb31924a6
Dilan Silva <sdilansilva@gmail.com>
2021-03-15 22:36:13 +0530

    Sign-Up page is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import SignIn from './pages/Sign-In';//import Sign In page
+import SignUp from './pages/Sign-Up';//import Sign up page
 
 function App() {
   return (
     <div>
-      <SignIn/>
+      <SignUp/>
     </div>
   );
 }
--- src/pages/Sign-Up.js
+++ src/pages/Sign-Up.js
@@ -1,9 +1,95 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,
+         Form,
+        Jumbotron,
+        Container,
+        Row,
+        Col,
+        Image,
+        Card } from 'react-bootstrap';
+
+import '../css/SignIn.css';
+import image from '../images/weather_2x.jpg';//import image
 
 export default function SignUp() {
+
+    const [name,setName] = useState('show');//state for button name
+    const [type,setType] = useState('password');//state for input type
+
+    const toggleButton = (e) => {
+        e.preventDefault();
+        if(name == 'show'){
+            setName('hide');
+            setType('text');
+        } else{
+            setName('show');
+            setType('password');
+        }
+    }
+
     return (
         <div>
-            
+        <Jumbotron className="backgroundColor" fluid>
+        <Container>
+            <Row>
+                <Col sm={7}>
+                    <Image
+                        src={image}
+                        className="imageBackGround"
+                    />
+                </Col>
+
+                <Col sm={5}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Text>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control 
+                                        type="email" 
+                                        placeholder="Enter Email"  
+                                    />
+                                    
+                                </Form.Group>
+                          
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control 
+                                        type={type} 
+                                        placeholder="Enter Password"
+                                    />
+                                        <button
+                                            class="button button5"
+                                            onClick={toggleButton}
+                                        >{name}</button>
+                                </Form.Group>
+
+                            <div class="textAlign">
+                                <Button 
+                                    size="sm"
+                                    variant="primary" 
+                                    type="submit"
+                                    block>
+                                 Log-In  
+                                </Button>
+                            </div>
+
+                          </Form>
+                                <br></br>
+                            <div className="textAlign"> 
+                                <a href="/signup" className="textSpace">Have an account? Log-In</a>
+                            </div>
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    </Jumbotron>
         </div>
     )
 }
