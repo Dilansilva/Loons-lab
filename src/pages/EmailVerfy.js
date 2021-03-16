5b1ba60e6de5739f041af5293145bab133acc448
Dilan Silva <sdilansilva@gmail.com>
2021-03-16 21:26:35 +0530

    Emial verification is done have to do more

--- src/pages/EmailVerfy.js
+++ src/pages/EmailVerfy.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form,
@@ -9,6 +9,54 @@ import '../css/SignIn.css';
 
 
 export default function EmailVerfy() {
+    const [code,setCode] = useState();//state for email verification code
+    const [wrongcode,setWrongcode] = useState();//state for wrong code
+
+    const [error,setError] = useState();//state for error
+
+    const [email,setEmail] = useState();//state for email
+
+    useEffect(() => {
+        fetch('http://localhost:4000/emailverify',{
+            method : 'POST',
+            mode : 'cors',
+            headers: {
+                   Accept : 'application/json',
+               'Content-Type' : 'application/json',
+               'Access-Control-Allow-Origin' : 'http://localhost:4000/emailverify'
+             }, 
+             body: JSON.stringify({
+               email : email
+           }),
+        })
+    },[])
+
+    const submit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:4000/emailverify',{
+            method : 'POST',
+            mode : 'cors',
+            headers: {
+                   Accept : 'application/json',
+               'Content-Type' : 'application/json',
+               'Access-Control-Allow-Origin' : 'http://localhost:4000/emailverify'
+             }, 
+             body: JSON.stringify({
+               code : code
+           }),
+        })
+        .then(response => response.json())
+        .then((data) => {
+               if(data.message === 'verify code'){
+                   //ROUTE HAS TO DONE HERE
+               } else if(data.message === 'wrong code'){
+                   setWrongcode('Wrong code');
+               }
+        })
+        .catch((error) => {
+            setError('Error');
+        })
+    }
     return (
         <div className="textAlign textSpace">
            
@@ -19,12 +67,19 @@ export default function EmailVerfy() {
                         <Form.Row>
                             <Col>
                                 <Form.Label>Check your email</Form.Label>
-                                    <Form.Control placeholder="Enter Email Verification Code" />
+                                    <Form.Control 
+                                        placeholder="Enter Email Verification Code"
+                                        onChange={(e) => {setCode(e.target.value)}} 
+                                    />
+                                    <small>{wrongcode}</small>
+                                    <small>{error}</small>
                             </Col>
                         </Form.Row>
 
                     </Form><br></br>
-                        <Button>Click</Button>
+                        <Button
+                            onClick={submit}
+                        >Click</Button>
                 </Jumbotron>
             </Col>
            
